01f85131dcc7212d6f96d7925460ad8f2b5c3cb0
subhashish <subhashish@qexon.com>
2019-04-12 19:52:13 +0530

    asd

--- app.js
+++ app.js
@@ -42,10 +42,10 @@ var functions = require('./config/intervalFunctions');
 
 functions.checkBookingExpired(db);
 
-// var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(PORT, '0.0.0.0', function () {
+  console.log('Everything is ill right on port %d!', PORT)
 });
 
 
